f838c72cfbb8aa7a53d53e143c25b2030586f5d2
Staś Małolepszy <stas@duzodobrze.pl>
2017-11-28 10:16:22 +0100

    Init 0.0.1

---
+++ background.js
@@ -0,0 +1,37 @@
+browser.contextMenus.create({
+    id: "detach-tabs-to-the-right",
+    title: "This Tab and Tabs to the Right",
+    contexts: ["tab"],
+    onclick: detach_tabs_to_the_right
+});
+
+browser.contextMenus.create({
+    id: "detach-tabs-same-origin",
+    title: "All Tabs from the Same Origin",
+    contexts: ["tab"],
+    onclick: detach_tabs_same_origin
+});
+
+async function detach_tabs_to_the_right(_, tab) {
+    const current_tabs = await browser.tabs.query({ currentWindow: true });
+    const ids_to_move = current_tabs
+        .sort((a, b) => a.index - b.index)
+        .slice(tab.index + 1)
+        .map(tab => tab.id);
+
+    const window = await browser.windows.create({ tabId: tab.id });
+    await browser.tabs.move(ids_to_move, { windowId: window.id, index: 1 });
+}
+
+async function detach_tabs_same_origin(_, tab) {
+    const url = new URL(tab.url);
+    const tab_origin = `${url.origin}/*`;
+
+    const current_tabs = await browser.tabs.query({ currentWindow: true, url: tab_origin });
+    const ids_to_move = current_tabs
+        .sort((a, b) => a.index - b.index)
+        .map(tab => tab.id);
+
+    const window = await browser.windows.create({ tabId: tab.id });
+    await browser.tabs.move(ids_to_move, { windowId: window.id, index: 1 });
+}
