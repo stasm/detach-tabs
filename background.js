5510412085e1a1447b495df11f793de4dcf5ac01
iwconfig <snelhingst@gmail.com>
2019-01-24 16:31:57 +0100

    Move tabs of same origin across windows to new window
    closes #4

--- background.js
+++ background.js
@@ -12,6 +12,13 @@ browser.contextMenus.create({
     onclick: detach_tabs_same_origin
 });
 
+browser.contextMenus.create({
+    id: "detach-tabs-same-origin-across-windows",
+    title: browser.i18n.getMessage("detach-same-origin-across-windows"),
+    contexts: ["tab"],
+    onclick: detach_tabs_same_origin_across_windows
+});
+
 async function detach_tabs_to_the_right(_, tab) {
     const current_tabs = await browser.tabs.query({ currentWindow: true });
     const ids_to_move = current_tabs
@@ -28,6 +35,19 @@ async function detach_tabs_same_origin(_, tab) {
     const tab_origin = `${url.origin}/*`;
 
     const current_tabs = await browser.tabs.query({ currentWindow: true, url: tab_origin });
+    const ids_to_move = current_tabs
+        .sort((a, b) => a.index - b.index)
+        .map(tab => tab.id);
+
+    const window = await browser.windows.create({ tabId: tab.id });
+    await browser.tabs.move(ids_to_move, { windowId: window.id, index: 1 });
+}
+
+async function detach_tabs_same_origin_across_windows(_, tab) {
+    const url = new URL(tab.url);
+    const tab_origin = `${url.origin}/*`;
+
+    const current_tabs = await browser.tabs.query({ url: tab_origin });
     const ids_to_move = current_tabs
         .sort((a, b) => a.index - b.index)
         .map(tab => tab.id);
